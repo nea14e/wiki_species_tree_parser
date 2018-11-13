0079313fb7a767189c97357472d23a62275a5a3d
Chkan <chkan@it-avangard.su>
2018-11-13 15:03:52 +0300

    Создание базы. Царства и зашитые данные для царств. Заполнение списка видов для парсинга - уже не падает.

--- db_functions.py
+++ db_functions.py
@@ -10,7 +10,7 @@ class DbFunctions:
     conn = None
 
     @staticmethod
-    def init_db(kingdom_table_name):
+    def init_db():
         # Подключаемся к базе данных по умолчанию, чтобы создать нашу базу, если надо
         general_conn = psycopg2.connect(
             "dbname='postgres' user='" + DbFunctions.user + "' host='" + DbFunctions.host + "' password='" + DbFunctions.password + "'")
@@ -31,24 +31,107 @@ class DbFunctions:
             DbFunctions.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
         # Создаём в ней таблицы, если надо
+        # Таблица с царствами
+        cur = DbFunctions.conn.cursor()
+        sql = "SELECT 1 FROM pg_class tbl WHERE tbl.relname = 'kingdoms';"
+        cur.execute(sql)
+        if cur.rowcount == 0:
+            cur = DbFunctions.conn.cursor()
+            sql = "CREATE TABLE public.kingdoms (" \
+                  "id serial NOT NULL" \
+                  ", title text NOT NULL" \
+                  ", href text NOT NULL" \
+                  ", CONSTRAINT pk_kingdoms PRIMARY KEY(id)" \
+                  ", CONSTRAINT uq_kingdoms_title UNIQUE (title)" \
+                  ", CONSTRAINT uq_kingdoms_href UNIQUE (href)" \
+                  ");"
+            print(str(sql))
+            cur.execute(sql)
+            cur = DbFunctions.conn.cursor()
+            sql = "INSERT INTO public.kingdoms (title, href) " \
+                  "VALUES ('animals', 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83')," \
+                  "       ('plants', 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A0%D0%B0%D1%81%D1%82%D0%B5%D0%BD%D0%B8%D1%8F_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83')," \
+                  "       ('mushrooms', 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%93%D1%80%D0%B8%D0%B1%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83')," \
+                  "       ('viruses', 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%92%D0%B8%D1%80%D1%83%D1%81%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83');"
+            print(str(sql))
+            cur.execute(sql)
+        # Таблица со списком
+        cur = DbFunctions.conn.cursor()
+        sql = "SELECT 1 FROM pg_class tbl WHERE tbl.relname = 'list';"
+        cur.execute(sql)
+        if cur.rowcount == 0:
+            cur = DbFunctions.conn.cursor()
+            sql = "CREATE TABLE public.list (" \
+                  "id bigserial NOT NULL" \
+                  ", kingdom_id int NOT NULL" \
+                  ", title text NOT NULL" \
+                  ", href text NOT NULL" \
+                  ", CONSTRAINT pk_list PRIMARY KEY(id)" \
+                  ", CONSTRAINT fk_list_kingdom FOREIGN KEY (kingdom_id) REFERENCES public.kingdoms(id)" \
+                  ", CONSTRAINT uq_list UNIQUE (kingdom_id, title)" \
+                  ");"
+            print(str(sql))
+            cur.execute(sql)
         # Таблица с подробностями
         cur = DbFunctions.conn.cursor()
         sql = "SELECT 1 FROM pg_class tbl WHERE tbl.relname = 'details';"
         cur.execute(sql)
         if cur.rowcount == 0:
             cur = DbFunctions.conn.cursor()
-            sql = "CREATE TABLE public.details (title text, href text);"
+            sql = "CREATE TABLE public.details (" \
+                  "id bigint NOT NULL" \
+                  ", title text NOT NULL" \
+                  ", type text NOT NULL" \
+                  ", image_url text" \
+                  ", parent_id bigint" \
+                  ", CONSTRAINT pk_details PRIMARY KEY(id)" \
+                  ", CONSTRAINT fk_details_id FOREIGN KEY(id) REFERENCES public.list(id)" \
+                  ", CONSTRAINT fk_details_parent_id FOREIGN KEY(parent_id) REFERENCES public.list(id)" \
+                  ");"
             print(str(sql))
             cur.execute(sql)
-        # Таблица со списком
+
+    @staticmethod
+    def get_kingdom_url(kingdom_title):
+        if not DbFunctions.conn:
+            raise Exception("Сначала вызовите метод DbFunctions.init_db()!")
+
+        # Узнаем id царства по его названию
         cur = DbFunctions.conn.cursor()
-        sql = "SELECT 1 FROM pg_class tbl WHERE tbl.relname = '" + str(kingdom_table_name) + "';"
+        sql = "SELECT href " \
+              "FROM public.kingdoms " \
+              "WHERE title = '" + str(kingdom_title) + "';"
+        print(str(sql))
         cur.execute(sql)
-        if cur.rowcount == 0:
-            cur = DbFunctions.conn.cursor()
-            sql = "CREATE TABLE public." + str(kingdom_table_name) + " (title text, href text);"
-            print(str(sql))
-            cur.execute(sql)
+        try:
+            kingdom_url = cur.fetchone()[0]
+            print('kingdom_url = ' + str(kingdom_url))
+        except:
+            raise Exception("Царство не найдено: " + str(kingdom_title))
+        return kingdom_url
 
+    @staticmethod
+    def add_list_item(kingdom_title, title, href):
+        if not DbFunctions.conn:
+            raise Exception("Сначала вызовите метод DbFunctions.init_db()!")
 
-DbFunctions.init_db('animals')
+        # Узнаем id царства по его названию
+        cur = DbFunctions.conn.cursor()
+        sql = "SELECT id " \
+              "FROM public.kingdoms " \
+              "WHERE title = '" + str(kingdom_title) + "';"
+        print(str(sql))
+        cur.execute(sql)
+        try:
+            kingdom_id = cur.fetchone()[0]
+        except:
+            raise Exception("Царство не найдено: " + str(kingdom_title))
+
+        # Добавляем новый элемент в список или обновляем уже существующий
+        cur = DbFunctions.conn.cursor()
+        sql = "INSERT INTO public.list (kingdom_id, title, href) " \
+              "VALUES ('" + str(kingdom_id) + "', '" + str(title) + "', '" + str(href) + "')" \
+                                                              "ON CONFLICT ON CONSTRAINT uq_list DO UPDATE SET href = EXCLUDED.href;"
+        print(str(sql))
+        cur.execute(sql)
+
--- wiki_parser.py
+++ wiki_parser.py
@@ -7,13 +7,66 @@ import time
 
 from selenium.common.exceptions import WebDriverException
 
+from db_functions import DbFunctions
+
+
+def populate_list_for_kingdom(kingdom_title):
+    kingdom_list_url = DbFunctions.get_kingdom_url(kingdom_title)
+
+    driver = webdriver.Firefox(executable_path=os.path.join(os.getcwd(), 'geckodriver'))
+    driver.get(kingdom_list_url)
+    time.sleep(1)
+    driver.implicitly_wait(2)
+    item_counter = 0
+
+    while True:  # Цикл перехода на след. страницу
+        try:
+            next_page = driver.find_element_by_xpath('//div[@id="mw-pages"]/a[2]').get_attribute(
+                "href")  # След. страница
+        except WebDriverException:
+            print('Все страницы списка обработаны')
+            break
+        # Сохраем в базу ссылки, чтобы потом по ним переходить
+        for link in driver.find_elements_by_xpath(
+                '//div[@class="mw-category-group"]/ul/li/a'):
+            try:
+                item_title = link.text  # Ссылка
+                item_details_href = link.get_attribute("href")  # Ссылка
+                print(
+                    'Новый элемент в списке для парсинга: %s, %s, %s' % (kingdom_title, item_title, item_details_href))
+                DbFunctions.add_list_item(kingdom_title, item_title, item_details_href)
+                item_counter += 1
+            except WebDriverException:
+                print('Ошибка:\n', traceback.format_exc())
+        driver.get(next_page)  # Переходим на след страницу
+    print('ЦАРСТВО ' + kingdom_list_url + ' БЫЛО ОБРАБОТАНО! Всего успешно ' + str(item_counter) + " элементов добавлено в список.")
+    driver.quit()
+
+
+def parse_details(kingdom_title):
+    kingdom_list_url = DbFunctions.get_kingdom_url(kingdom_title)
+    # for link in links_mas:  # Цикл по "массиву ссылок на странице"
+    # try:
+    #     driver.get(link)  # TODO Переход по ссылке
+    #     print('\n')
+    #     print('===========================================')
+    #     print('ССЫЛКА: ' + str(link))
+    #     get_levels(driver)  # TODO Парсинг информации
+    #     item_counter += 1
+    #     time.sleep(1)
+    # except WebDriverException:
+    #     print('Ошибка:\n', traceback.format_exc())
+
+
+
 def get_levels(driver):
-    # TODO по ссылкам где нет кнопки "показать" пропускает информацию(Скорей всего там другая структура HTML), сделать реплейс <b> и </b> в результате на пустоту
+    # TODO по ссылкам где нет кнопки "показать" пропускает информацию(Скорей всего там другая структура HTML),
+    # TODO сделать реплейс <b> и </b> в результате на пустоту
     parsed_levels = []
     infobox = driver.find_element_by_xpath('//table[@class="infobox"]')
     levels = infobox.find_elements_by_xpath('.//div[@class="NavFrame collapsed"]/div')
     for level in reversed(levels):
-        if level != levels[0]: #Если 1 элемент(ненужный) то пропускаем
+        if level != levels[0]:  # Если 1 элемент(ненужный) то пропускаем
             parsed_level = ParsedLevel()
             category_html = level.find_element_by_xpath('.//td[1]')
             try:
@@ -32,7 +85,8 @@ def get_levels(driver):
                     parsed_level.value = value_html.text
             print(parsed_level.category + ' ' + parsed_level.value)
             parsed_levels.append(parsed_level)
-        else: pass
+        else:
+            pass
     return parsed_levels
 
 
@@ -40,51 +94,6 @@ class ParsedLevel:
     pass
 
 
-def parse_list_for_kingdom(kingdom_list_url):
-    links_mas = [] # Массив ссылок на странице
-    driver = webdriver.Firefox(executable_path=os.path.join(os.getcwd(), 'geckodriver'))
-    driver.get(kingdom_list_url)
-    time.sleep(1)
-    driver.implicitly_wait(2)
-    item_counter = 0
-
-    while True: # Цикл перехода на след. страницу
-        try:
-            next_page = driver.find_element_by_xpath('//div[@id="mw-pages"]/a[2]').get_attribute("href") # След. страница
-        except:
-            print('Все страницы списка обработаны')
-            break
-        for link in driver.find_elements_by_xpath('//div[@class="mw-category-group"]/ul/li/a'): #Цикл заполнения "массива ссылок на странице" ссылками, чтобы по ним переходить
-            try:
-                details_page_url = link.get_attribute("href") #Ссылка
-                links_mas.append(details_page_url) #Добовляем ссылку в массив
-            except:
-                print('Ошибка:\n', traceback.format_exc())
-        for link in links_mas: #Цикл по "массиву ссылок на странице"
-            try:
-                driver.get(link) #Переход по ссылке
-                print('\n')
-                print('===========================================')
-                print('ССЫЛКА: ' + str(link))
-                get_levels(driver) #Парсинг информации
-                item_counter += 1
-                time.sleep(1)
-            except WebDriverException:
-                print('Ошибка:\n', traceback.format_exc())
-        driver.get(next_page) #Переходим на след страницу
-        links_mas = [] #Чистим "массив ссылок на странице" для повтороной итерации
-    print('ЦАРСТВО ' + kingdom_list_url + ' БЫЛО ОБРАБОТАНО!')
-    driver.quit()
-
-kingdom_animals_url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0' \
-                      '%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1' \
-                      '%82%D1%83'
-kingdom_plants_url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A0%D0' \
-                     '%B0%D1%81%D1%82%D0%B5%D0%BD%D0%B8%D1%8F_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82' \
-                     '%D1%83'
-kingdom_mushrooms_url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%93' \
-                        '%D1%80%D0%B8%D0%B1%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83'
-kingdom_viruses_url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%92%D0' \
-                      '%B8%D1%80%D1%83%D1%81%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83'
 # Выберите нужное и подставьте сюда перед запуском
-parse_list_for_kingdom(kingdom_animals_url)
+DbFunctions.init_db()
+populate_list_for_kingdom('animals')
