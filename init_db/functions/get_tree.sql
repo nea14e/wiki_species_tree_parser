ea0bd15804baaaefee76f418bad4000e80cd414c
Nea14 <nea14e@gmail.com>
2020-07-05 16:35:26 +0300

    get_tree() - добавлен флаг "Этот уровень содержит элемент, выбранный пользователем". Чтобы потом отчёркивать этот уровень линиями пожирнее.

--- init_db/functions/get_tree.sql
+++ init_db/functions/get_tree.sql
@@ -3,15 +3,16 @@ CREATE OR REPLACE FUNCTION public.get_tree(_id bigint DEFAULT NULL::bigint, _lan
 AS
 $$
 DECLARE
-  _answer_json       jsonb = '[]'::jsonb; -- jsonb is stored in pre-parsed way so operations with him are more faster
-  _level_items_json  jsonb = '[]'::jsonb;
-  _level_object_json jsonb = '{}'::jsonb;
-  _next_parent_id    bigint;
-  _cur_parent_id     bigint;
-  _prev_parent_id    bigint;
-  _level_max_order   int;
-  _level_min_order   int;
-  _cur_rank          record;
+  _answer_json             jsonb = '[]'::jsonb; -- jsonb is stored in pre-parsed way so operations with him are more faster
+  _level_items_json        jsonb = '[]'::jsonb;
+  _level_object_json       jsonb = '{}'::jsonb;
+  _has_level_selected_item boolean;
+  _next_parent_id          bigint;
+  _cur_parent_id           bigint;
+  _prev_parent_id          bigint;
+  _level_max_order         int;
+  _level_min_order         int;
+  _cur_rank                record;
 BEGIN
   IF _id IS NULL THEN
     -- When page is opened with default routing, show first 3 levels fully
@@ -19,7 +20,7 @@ BEGIN
     -- Cycle by _cur_level_type:
     FOR _cur_rank IN
       SELECT "type"                                                  AS "type",
-             COALESCE(titles_by_languages ->> _language_key, "type") AS title_for_language  -- Latin type if not present
+             COALESCE(titles_by_languages ->> _language_key, "type") AS title_for_language -- Latin type if not present
       FROM public.ranks
       ORDER BY "order" DESC -- we are going from top level to bottom
       LIMIT 3 -- show 3 highest levels
@@ -30,7 +31,7 @@ BEGIN
         SELECT jsonb_agg(t ORDER BY title_for_language) INTO _level_items_json
         FROM (
                SELECT id,
-                      COALESCE(titles_by_languages ->> _language_key, title) AS title_for_language, -- Latin name if not present
+                      COALESCE(titles_by_languages ->> _language_key, title) AS title_for_language,    -- Latin name if not present
                       page_url,
                       image_url,
                       COALESCE(wikipedias_by_languages ->> _language_key,
@@ -48,7 +49,8 @@ BEGIN
         _level_object_json = jsonb_build_object(
             'type', _cur_rank."type",
             'title_on_language', _cur_rank.title_for_language,
-            'items', _level_items_json
+            'items', _level_items_json,
+            'is_level_has_selected_item', FALSE
           );
         -- RAISE NOTICE '_level_json: object: ''%''.', _level_json;
 
@@ -85,7 +87,7 @@ BEGIN
         -- Cycle by _cur_level_type:
         FOR _cur_rank IN
           SELECT "type"                                                  AS "type",
-                 COALESCE(titles_by_languages ->> _language_key, "type") AS title_for_language  -- Latin type if not present
+                 COALESCE(titles_by_languages ->> _language_key, "type") AS title_for_language -- Latin type if not present
           FROM public.ranks
           WHERE ("order" < _level_max_order OR _level_max_order IS NULL) -- load items with all highest levels when current level's "_cur_parent_id IS NULL"
             AND "order" >= _level_min_order
@@ -94,14 +96,16 @@ BEGIN
 
             -- RAISE NOTICE 'NEXT _cur_level_type: ''%''.', _cur_level_type;
 
-            SELECT jsonb_agg(t ORDER BY title_for_language) INTO _level_items_json
+            SELECT jsonb_agg(t ORDER BY title_for_language),
+                   SUM(CASE WHEN t.is_selected THEN 1 ELSE 0 END)
+                   INTO _level_items_json, _has_level_selected_item
             FROM (
                    SELECT id,
-                          COALESCE(titles_by_languages ->> _language_key, title) AS title_for_language,  -- Latin name if not present
+                          COALESCE(titles_by_languages ->> _language_key, title) AS title_for_language,    -- Latin name if not present
                           page_url,
                           image_url,
                           COALESCE(wikipedias_by_languages ->> _language_key,
-                                   wikipedias_by_languages ->> 'en')             AS wiki_url_for_language,  -- English type if not present
+                                   wikipedias_by_languages ->> 'en')             AS wiki_url_for_language, -- English type if not present
                           parent_id,
                           COALESCE((id = _prev_parent_id)::boolean OR (id = _id)::boolean,
                                    FALSE)                                        AS is_expanded,
@@ -120,7 +124,8 @@ BEGIN
               _level_object_json = jsonb_build_object(
                   'type', _cur_rank."type",
                   'title_on_language', _cur_rank.title_for_language,
-                  'items', _level_items_json
+                  'items', _level_items_json,
+                  'is_level_has_selected_item', _has_level_selected_item
                 );
 
               -- RAISE NOTICE '_level_json: object: ''%''.', _level_json;
