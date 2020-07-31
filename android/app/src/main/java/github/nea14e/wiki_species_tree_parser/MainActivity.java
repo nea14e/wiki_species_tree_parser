fc33c02e461ceabf9aff0b63c2879cd194be934a
nozdrev <nozdrev@mail.it-avangard.su>
2020-07-31 18:00:38 +0300

    Android: just created empty project

---
+++ android/app/src/main/java/github/nea14e/wiki_species_tree_parser/MainActivity.java
@@ -0,0 +1,14 @@
+package github.nea14e.wiki_species_tree_parser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
