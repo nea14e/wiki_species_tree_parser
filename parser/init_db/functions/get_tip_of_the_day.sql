32d410d277b1c08a913108021ab1f6b3db14ec36
Nea14 <nea14e@gmail.com>
2020-08-01 10:45:42 +0300

    DB: совет дня для языка, на котором нет советов, раньше выбирал случайную строку изо всей БД и выдавал NULL. Теперь он выбирает случайную строку на английском и выдаёт английский.

--- parser/init_db/functions/get_tip_of_the_day.sql
+++ parser/init_db/functions/get_tip_of_the_day.sql
@@ -4,19 +4,32 @@ CREATE OR REPLACE FUNCTION public.get_tip_of_the_day(_language_key text)
   LANGUAGE SQL
 AS
 $$
-  SELECT json_agg(t)
-  FROM (
+  WITH translated_cte AS (
     SELECT
-           id,
-           tip_on_languages ->> _language_key AS tip_text
+      id,
+      tip_on_languages ->> _language_key AS tip_text
     FROM public.tips_of_the_day
-    WHERE (  -- get translated tips only or any tips (when translated tips are not present for chosen language at all)
-        tip_on_languages ? _language_key
-        OR NOT EXISTS(SELECT 1
-                      FROM public.tips_of_the_day
-                      WHERE tip_on_languages ? _language_key)
-      )
+    WHERE tip_on_languages ? _language_key  -- get translated tips if exists
     ORDER BY random()
     LIMIT 1
+  ),
+  english_cte AS (
+    SELECT
+      t.id,
+      t.tip_on_languages ->> 'en' AS tip_text
+    FROM public.tips_of_the_day t
+      LEFT JOIN translated_cte ON TRUE
+    WHERE t.tip_on_languages ? 'en'
+      AND translated_cte.id IS NULL  -- else get any tips on English
+    ORDER BY random()
+    LIMIT 1
+  )
+  SELECT json_agg(t)
+  FROM (
+    SELECT id, tip_text
+    FROM translated_cte
+    UNION ALL
+    SELECT id, tip_text
+    FROM english_cte
   ) t;
 $$;
