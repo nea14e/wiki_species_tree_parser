b29a3d785eea6406b6bfa78502de40024c9bddc2
Nea14 <nea14e@gmail.com>
2020-10-11 22:45:29 +0300

    Front: вынес адрес бэкенда в файл environment

--- frontend/src/app/network.service.ts
+++ frontend/src/app/network.service.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Tree} from './models';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
-  private BACKEND_API_URL = 'http://127.0.0.1:8000/api/';
-
   constructor(private http: HttpClient) { }
 
   public getTreeDefault(): Observable<Tree> {
-    return this.http.get<Tree>(this.BACKEND_API_URL + 'get_tree_default');
+    return this.http.get<Tree>(environment.BACKEND_API_URL + 'get_tree_default');
   }
 
 }
