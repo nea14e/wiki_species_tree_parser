c06421393129e1576441aea103ebe9be5150c7e5
Nea14 <nea14e@gmail.com>
2020-10-13 14:01:07 +0300

    Front: стили для раскрытия - 1/2 (TODO стрелка вниз, границы выбранного уровня синим, кнопки выбранного элемента)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NetworkService} from './network.service';
-import {Tree} from './models';
+import {Item, Tree} from './models';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +19,26 @@ export class AppComponent implements OnInit {
     }, error => {
       alert(error);
     });
+  }
+
+  getItemClass(item: Item): string {
+    if (item.is_selected === true) {
+      return 'selected';
+    } else if (item.is_expanded === true) {
+      return 'expanded';
+    }
+    return '';
+  }
+
+  onItemClick(item: Item): void {
+    if (item.is_expanded === false) {
+      item.is_expanded = true;
+    } else if (item.is_selected === false) {
+      item.is_selected = true;
+    } else {
+      item.is_expanded = false;
+      item.is_selected = false;
+    }
   }
 
 }
