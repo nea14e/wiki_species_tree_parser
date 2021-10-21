c37d0dcdb59fbd8e7a8bd65c74358eb9bd809c8e
artem <temakhromov@yandex.ru>
2021-10-21 22:43:45 +0300

    Избранное - заголовок, фон

--- frontend/src/app/favorites/favorites.component.ts
+++ frontend/src/app/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RootDataKeeperService} from '../root-data-keeper.service';
 
 @Component({
   selector: 'app-favorites',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public rootData: RootDataKeeperService) { }
 
   ngOnInit(): void {
   }
