15140d2945de8f39e92bbd6037f847c543b6dee5
artem <temakhromov@yandex.ru>
2021-12-05 20:26:00 +0300

    Избранное - переход в дерево по нажатию

--- frontend/src/app/favorites/favorites.component.ts
+++ frontend/src/app/favorites/favorites.component.ts
@@ -3,6 +3,7 @@ import {RootDataKeeperService} from '../root-data-keeper.service';
 import {FavoritesItem} from '../models';
 import {NetworkService} from '../network.service';
 import {FavoritesService} from '../favorites.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
@@ -14,7 +15,8 @@ export class FavoritesComponent implements OnInit {
 
   constructor(public rootData: RootDataKeeperService,
               private networkService: NetworkService,
-              public favoritesService: FavoritesService
+              public favoritesService: FavoritesService,
+              private router: Router
               ) { }
 
   ngOnInit(): void {
@@ -22,5 +24,9 @@ export class FavoritesComponent implements OnInit {
 
   onDeleteItemClick(item: FavoritesItem): void {
     this.favoritesService.deleteItem(item.id);
+  }
+
+  toTree(id: number): void {
+      this.router.navigate(['tree'], {queryParams: { id } });
   }
 }
