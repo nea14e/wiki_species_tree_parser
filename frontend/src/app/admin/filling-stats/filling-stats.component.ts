0cdd801cb1b71a497b0cc34313802298b1af3cef
artem <temakhromov@yandex.ru>
2022-01-05 23:51:23 +0300

    Таблица заполненности - HTML

--- frontend/src/app/admin/filling-stats/filling-stats.component.ts
+++ frontend/src/app/admin/filling-stats/filling-stats.component.ts
@@ -14,6 +14,11 @@ export class FillingStatsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // this.activatedRoute.queryParams.subscribe(params => {
+    //   // Эта лямбда будет вызвана автоматически при любых изменениях параметров адреса в браузере
+    //   this.groupNumber = +params.groupNumber || null;  // Приводим к числу с помощью + или, если в адресе не указан параметр groupNumber, то null.
+    //   this.nestedLevel = +params.nestedLevel || 0;  // Приводим к числу с помощью + или, если в адресе не указан параметр nestedLevel, то 0.
+    //   this.reload();
+  //   });
   }
-
 }
