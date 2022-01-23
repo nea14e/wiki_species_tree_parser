fc7c86e04717fbdae8b3216640a9d7c45b3ea527
artem <temakhromov@yandex.ru>
2022-01-23 19:08:46 +0300

    Таблица заполненности - пред прикручиванием бэкенда

--- frontend/src/app/admin/filling-stats/filling-stats.component.ts
+++ frontend/src/app/admin/filling-stats/filling-stats.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-filling-stats',
@@ -10,15 +11,28 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FillingStatsComponent implements OnInit {
 
-  constructor() {
-  }
+  groupsCount = 20;
+  groupNumber = null;
+  nestedLevel = 0;
+
+  constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // this.activatedRoute.queryParams.subscribe(params => {
-    //   // Эта лямбда будет вызвана автоматически при любых изменениях параметров адреса в браузере
-    //   this.groupNumber = +params.groupNumber || null;  // Приводим к числу с помощью + или, если в адресе не указан параметр groupNumber, то null.
-    //   this.nestedLevel = +params.nestedLevel || 0;  // Приводим к числу с помощью + или, если в адресе не указан параметр nestedLevel, то 0.
-    //   this.reload();
-  //   });
+    this.activatedRoute.queryParams.subscribe(params => {
+      // Эта лямбда будет вызвана автоматически при любых изменениях параметров адреса в браузере
+    // this.groupNumber = +this.params.groupNumber || null;
+    // Приводим к числу с помощью + или, если в адресе не указан параметр groupNumber, то null.
+    // this.nestedLevel = +this.params.nestedLevel || 0;
+    // Приводим к числу с помощью + или, если в адресе не указан параметр nestedLevel, то 0.
+    });
+    this.reload();
+  }
+
+  public reload(): void {
+
+  }
+
+  onGroupsCountChanged(e): void {
+    this.groupsCount = e.target.value;
   }
 }
