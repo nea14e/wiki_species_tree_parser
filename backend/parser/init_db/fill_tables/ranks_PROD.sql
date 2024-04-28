b43f9a8988e3f87a6481860a692a7c4ee36f12ce
Евгений Ноздрев <nea14e@gmail.com>
2024-04-28 17:11:42 +0300

    Таблица рангов - заполнение 1/2 - с большой буквы

--- backend/parser/init_db/fill_tables/ranks_PROD.sql
+++ backend/parser/init_db/fill_tables/ranks_PROD.sql
@@ -1,94 +1,94 @@
 TRUNCATE public.ranks;
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('magnordo', -9060, '{"ru": "магнотряд"}');
+VALUES ('Magnordo', -9060, '{"ru": "магнотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('superordo', -9070, '{"ru": "надотряд"}');
+VALUES ('Superordo', -9070, '{"ru": "надотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('grandordo', -9080, '{"ru": "грандотряд"}');
+VALUES ('Grandordo', -9080, '{"ru": "грандотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('mirordo', -9090, '{"ru": "миротряд"}');
+VALUES ('Mirordo', -9090, '{"ru": "миротряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('ordo', -9100, '{"ru": "отряд"}');
+VALUES ('Ordo', -9100, '{"ru": "отряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subordo', -9110, '{"ru": "подотряд"}');
+VALUES ('Subordo', -9110, '{"ru": "подотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('infraordo', -9120, '{"ru": "инфраотряд"}');
+VALUES ('Infraordo', -9120, '{"ru": "инфраотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('parvordo', -9130, '{"ru": "парвотряд"}');
+VALUES ('Parvordo', -9130, '{"ru": "парвотряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('suprafamilia', -9280, '{"ru": "надсемейство"}');
+VALUES ('Suprafamilia', -9280, '{"ru": "надсемейство"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('epifamilia', -9290, '{"ru": "эписемейство"}');
+VALUES ('Epifamilia', -9290, '{"ru": "эписемейство"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('familia', -9300, '{"ru": "семейство"}');
+VALUES ('Familia', -9300, '{"ru": "семейство"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subfamilia', -9310, '{"ru": "подсемейство"}');
+VALUES ('Subfamilia', -9310, '{"ru": "подсемейство"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('infrafamilia', -9320, '{"ru": "инфрасемейство"}');
+VALUES ('Infrafamilia', -9320, '{"ru": "инфрасемейство"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('supratribus', -9390, '{"ru": "надтриба"}');
+VALUES ('Supratribus', -9390, '{"ru": "надтриба"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('tribus', -9400, '{"ru": "триба"}');
+VALUES ('Tribus', -9400, '{"ru": "триба"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subtribus', -9410, '{"ru": "подтриба"}');
+VALUES ('Subtribus', -9410, '{"ru": "подтриба"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('infratribus', -9420, '{"ru": "инфратриба"}');
+VALUES ('Infratribus', -9420, '{"ru": "инфратриба"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('genus', -9500, '{"ru": "род"}');
+VALUES ('Genus', -9500, '{"ru": "род"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subgenus', -9510, '{"ru": "подрод"}');
+VALUES ('Subgenus', -9510, '{"ru": "подрод"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('supersectio', -9590, '{"ru": "надсекция"}');
+VALUES ('Supersectio', -9590, '{"ru": "надсекция"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('sectio', -9600, '{"ru": "секция"}');
+VALUES ('Sectio', -9600, '{"ru": "секция"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subsectio', -9610, '{"ru": "подсекция"}');
+VALUES ('Subsectio', -9610, '{"ru": "подсекция"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('series', -9700, '{"ru": "ряд"}');
+VALUES ('Series', -9700, '{"ru": "ряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subseries', -9710, '{"ru": "подряд"}');
+VALUES ('Subseries', -9710, '{"ru": "подряд"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('supraspecies', -9790, '{"ru": "надвид"}');
+VALUES ('Supraspecies', -9790, '{"ru": "надвид"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('species', -9800, '{"ru": "вид"}');
+VALUES ('Species', -9800, '{"ru": "вид"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subspecies', -9810, '{"ru": "подвид"}');
+VALUES ('Subspecies', -9810, '{"ru": "подвид"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('varietas', -9900, '{"ru": "разновидность"}');
+VALUES ('Varietas', -9900, '{"ru": "разновидность"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subvarietas', -9910, '{"ru": "подразновидность"}');
+VALUES ('Subvarietas', -9910, '{"ru": "подразновидность"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('forma', -10000, '{"ru": "форма"}');
+VALUES ('Forma', -10000, '{"ru": "форма"}');
 
 INSERT INTO public.ranks (type, "order", titles_by_languages)
-VALUES ('subforma', -10010, '{"ru": "подформа"}');
+VALUES ('Subforma', -10010, '{"ru": "подформа"}');
