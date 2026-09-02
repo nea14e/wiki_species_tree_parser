764fa144dc793d6e0994674879fe0ddd67e966bc
Мы-прогер <enozdrev@yandex.ru>
2026-09-02 13:54:28 +0300

    angular2 - SearchComponent - TODO spinner

---
+++ angular2/src/app/search/search.component.ts
@@ -0,0 +1,124 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {SearchItem} from '../models/search-item';
+import {RootDataKeeperService} from '../common/root-data-keeper.service';
+import {NetworkService} from '../network.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CopyToClipboardService} from '../common/copy-to-clipboard.service';
+import {debounceTime, distinctUntilChanged, Subject} from 'rxjs';
+import {Location} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {SpinnersAngularModule} from "spinners-angular";
+
+@Component({
+    selector: 'app-search.component',
+    imports: [
+        FormsModule,
+        SpinnersAngularModule
+    ],
+    templateUrl: './search.component.html',
+    styleUrl: './search.component.css',
+})
+export class SearchComponent implements OnInit {
+
+    query = '';
+    minQueryLength = 3;
+    attachToTipId: number | null = null;
+    queryChanged: Subject<string> = new Subject<string>();
+    resultItems: SearchItem[] = [];
+    isLoading = false;
+    isMore = false;
+    ITEMS_COUNT_BY_QUERY = 10;
+
+    rootData = inject(RootDataKeeperService);
+    private networkService = inject(NetworkService);
+    private activatedRoute = inject(ActivatedRoute);
+    private router = inject(Router);
+    private location = inject(Location);
+    private copyToClipboardService = inject(CopyToClipboardService);
+
+    ngOnInit(): void {
+        this.queryChanged  // see https://stackoverflow.com/a/52977862/7573844
+            .pipe(
+                debounceTime(1000), // wait 1 sec after the last event before emitting last event
+                distinctUntilChanged() // only emit if value is different from previous value
+            )
+            .subscribe(data => {
+                console.log('search:', data);
+                // Call your function which calls API or do anything you would like do after a lag of 1 sec
+                // noinspection JSIgnoredPromiseFromCall
+                this.router.navigate(
+                    ['search'],
+                    {
+                        queryParams: {q: data},
+                        replaceUrl: true,
+                        queryParamsHandling: 'merge'
+                    }
+                );
+            });
+
+        this.activatedRoute.queryParams.subscribe(params => {
+            this.rootData.lastSearchParams = params;
+            this.query = params['q'] || '';
+            this.attachToTipId = !!params['attachToTipId']
+                ? +params['attachToTipId']
+                : null;
+            this.runSearch(0);
+        });
+    }
+
+    runSearch(offset: number): void {
+        if (this.query.length < this.minQueryLength) {
+            this.resultItems = [];
+            return;
+        }
+
+        this.isLoading = true;
+        this.networkService.search(this.query, this.ITEMS_COUNT_BY_QUERY, offset)
+            .subscribe(result => {
+                console.log('result:', result);
+                this.isMore = result.length > this.ITEMS_COUNT_BY_QUERY;
+                if (this.isMore) {
+                    result.splice(this.ITEMS_COUNT_BY_QUERY);
+                }
+                console.log('modified result:', result);
+                if (offset > 0) {
+                    this.resultItems.push(...result);
+                } else {
+                    this.resultItems = result;
+                }
+                this.isLoading = false;
+            }, () => {
+                alert(this.rootData.translationRoot()?.translations.network_error);
+                this.isLoading = false;
+            });
+    }
+
+    onItemClick(item: SearchItem): void {
+        this.router.navigate(['tree'], {queryParams: {id: item.id}});
+    }
+
+    onShareClick(): void {
+        const val = window.location.href;
+        this.copyToClipboardService.copy(val);
+        alert(this.rootData.translationRoot()?.translations.link_copied);
+    }
+
+    onQueryInputChanged(inputText: string): void {
+        this.queryChanged.next(inputText);
+    }
+
+    attachToTree(item: SearchItem): void {
+        this.router.navigate(['admin/tip-translation'],
+            {queryParams: {speciesPageUrl: item.page_url, tipId: this.attachToTipId}}
+        );
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+
+    loadMore(): void {
+        const newOffset = this.resultItems.length;
+        this.runSearch(newOffset);
+    }
+}
